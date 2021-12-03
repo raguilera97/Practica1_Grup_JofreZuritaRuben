f76d61d787ef97a079e7775b8ef2e3d34f89de7b
MarcZurita <valerozurita@gmail.com>
2021-12-03 12:59:57 +0100

    Player movement acabado y animaciones basicas del movimiento

--- Practica1-Jofre-Ruben-Marc/Assets/Scripts/PlayerController.cs
+++ Practica1-Jofre-Ruben-Marc/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
-    
-    private float velocity = 4f;
+    Animator anim;
+
+    private float velocity = 2f;
     private float xInput, yInput;
     private Enums.Direction dir;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     
@@ -21,11 +23,42 @@ public class PlayerController : MonoBehaviour
     {
         CheckInput();
         ApplyMovement();
+        UpdateAnimation();
+    }
+
+
+    private void UpdateAnimation()
+    {
+        anim.SetFloat("X", xInput);
+        anim.SetFloat("Y", yInput);
+
+        anim.SetBool("isRunning", Math.Abs(xInput) >= 0.1f || Math.Abs(yInput) >= 0.1f);
+
+        if(CheckDirection() == Enums.Direction.West)
+        {
+            anim.SetFloat("LookX", -1.0f);
+            anim.SetFloat("LookY", 0.0f);
+        }
+        else if(CheckDirection() == Enums.Direction.East)
+        {
+            anim.SetFloat("LookX", 1.0f);
+            anim.SetFloat("LookY", 0.0f);
+        }
+        else if (CheckDirection() == Enums.Direction.North)
+        {
+            anim.SetFloat("LookX", 0.0f);
+            anim.SetFloat("LookY", 1.0f);
+        }
+        else if (CheckDirection() == Enums.Direction.South)
+        {
+            anim.SetFloat("LookX", 0.0f);
+            anim.SetFloat("LookY", -1.0f);
+        }
     }
 
     private void ApplyMovement()
     {
-        rb.velocity = new Vector2(xInput * velocity,yInput*velocity);
+        rb.velocity = new Vector2(xInput * velocity, yInput*velocity);
     }
 
     private void CheckInput()
@@ -45,11 +78,11 @@ public class PlayerController : MonoBehaviour
         {
             dir = Enums.Direction.South;
         }
-        else if (xInput == 1)
+        else if (xInput == -1)
         {
             dir = Enums.Direction.West;
         }
-        else if (xInput == -1)
+        else if (xInput == 1)
         {
             dir = Enums.Direction.East;
         }
