6a1f349d089fd80d3a7e0365f3be11e653072776
JDiazNoN <jofredima99@gmail.com>
2021-12-17 08:38:37 +0100

    Tilemap Base

---
+++ Practica1-Jofre-Ruben-Marc/Assets/Scripts/TilemapManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TilemapManager : MonoBehaviour
+{
+    [SerializeField] private Tilemap map;
+
+    [SerializeField] TileInfo[] tileInfos;
+
+    Dictionary<TileBase, TileInfo> infoFromTiles;
+
+    private void Awake()
+    {
+        infoFromTiles = new Dictionary<TileBase, TileInfo>();
+        PopulateDictionary();
+    }
+
+    private void PopulateDictionary()
+    {
+        foreach (TileInfo tileInfo in tileInfos)
+        {
+            foreach (TileBase tile in tileInfo.tiles)
+            {
+                infoFromTiles.Add(tile, tileInfo);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3Int gridPosition = map.WorldToCell(mousePosition);
+
+            TileBase clickedTile = map.GetTile(gridPosition);
+
+            TileInfo clickedTileInfo = infoFromTiles[clickedTile];
+
+            Debug.Log(clickedTile.name + " Speed Multiplaier: " + clickedTileInfo.velocityMultiplier);
+        }
+    }
+
+}
